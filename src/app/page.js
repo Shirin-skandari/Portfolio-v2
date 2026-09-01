0171ca2775b7e20df0e6ebbbb51546e66dc1ed1c
Shirin <shirin.eskandari@outlook.com>
2026-09-01 19:03:08 +0330

    feat: build and style hero section

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,28 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <main className={styles.home}>
-      <h1>Shirin Skandari</h1>
-      <p>Web Designer</p>
-      <p>Frontend Developer</p>
-      <h2>Building ideas into reality</h2>
-      <p>I create clean, modern and interactive websites.</p>
+      <div className={styles.hero}>
+        <p className={styles.greeting}>Hi, I'm</p>
+        <h1 className={styles.name}>Shirin Skandari</h1>
+        <div className={styles.roles}>
+          <p>Web Designer</p>
+          <p>Frontend Developer</p>
+        </div>
+          
+        
+        <h2 className={styles.tagline}>
+          Building ideas into reality
+        </h2>
+        <p className={styles.description}>
+          I create clean, modern and interactive websites.
+        </p>
+        <div className={styles.homeButtons}>
+          <button>View My Work</button>
+          <button>About Me</button>
+        </div>
+
+      </div>
+      
     </main>
   );
 }
